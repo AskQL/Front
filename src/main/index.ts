11279dfe82257c741bf542000ea149e93f126f7b
나형진 <skgudwls@konkuk.ac.kr>
2025-07-15 21:23:10 +0900

    fix: 하드웨어 가속 비활성화, 일부 경로 수정

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,6 +3,8 @@ import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
+app.disableHardwareAcceleration()
+
 function createWindow(): void {
   const mainWindow = new BrowserWindow({
     width: 900,
@@ -12,7 +14,7 @@ function createWindow(): void {
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      sandbox: true
     }
   })
 
